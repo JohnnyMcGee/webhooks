75693dab48d5efa349870fc768809158ada2398b
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-10-17 08:03:00 -0500

    Add function

---
+++ function.go
@@ -0,0 +1,37 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io"
+	"log"
+	"net/http"
+)
+
+var defaultMessage = "Cowabunga, dude!"
+
+func main(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Message string `json:"message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		switch err {
+		case io.EOF:
+			fmt.Fprint(w, defaultMessage)
+			return
+		default:
+			log.Printf("json.NewDecoder: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+	}
+
+	if d.Message == "" {
+		fmt.Fprint(w, defaultMessage)
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
+}
